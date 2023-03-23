306aeeb356563472b5b011f4004295f50b60a32e
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-03-23 21:40:37 +0100

    show shapes

--- script.js
+++ script.js
@@ -11,4 +11,18 @@ function fillShape(id){
 
     fields[id] = currentShape;
     console.log(fields);
+    draw();
+}
+
+function draw(){
+    for(let i=0; i < fields.length; i++){
+        if (fields[i] == 'circle') {
+            document.getElementById('circle-' + i).classList.remove('d-none');
+        }
+
+        if (fields[i] == 'cross') {
+            document.getElementById('cross-' + i).classList.remove('d-none');
+        }
+    }
+
 }
