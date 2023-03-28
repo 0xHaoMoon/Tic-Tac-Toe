cf9c7b76421376b726fc4623b7aac13db67df595
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-03-28 19:20:44 +0200

    animated lines

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ let fields = [];
 let currentShape = 'cross';
 
 function fillShape(id){
+    if(!fields[id]){
     if (currentShape == 'cross') {
         currentShape = 'circle';
+        document.getElementById('player-1').classList.remove('player-inactive')
+        document.getElementById('player-2').classList.add('player-inactive')
     } else {
         currentShape = 'cross';
+        document.getElementById('player-2').classList.remove('player-inactive')
+        document.getElementById('player-1').classList.add('player-inactive')
     }
 
     fields[id] = currentShape;
     console.log(fields);
     draw();
     checkForWin();
+}
 }
 
 function draw(){
@@ -32,34 +38,47 @@ function checkForWin(){
 
     if (fields[0]== fields[1] && fields[1] == fields[2] && fields[0]){
         winner = fields[0];
+        document.getElementById('linie-0').style.transform='scaleX(1)';
     }
 
     if (fields[3]== fields[4] && fields[4] == fields[5] && fields[3] ){
         winner = fields[3];
+        document.getElementById('linie-1').style.transform='scaleX(1)';
     }
 
-    if (fields[7]== fields[8] && fields[8] == fields[9] && fields[7]){
+    if (fields[6]== fields[7] && fields[7] == fields[8] && fields[7]){
         winner = fields[7];
+        document.getElementById('linie-2').style.transform='scaleX(1)';
     }
 
     if (fields[0]== fields[3] && fields[3] == fields[6] && fields[0]){
         winner = fields[0];
+        document.getElementById('linie-4').style.transform='scaleX(1) rotate(90deg)';
+
     }
 
     if (fields[1]== fields[4] && fields[4] == fields[7] && fields[1]){
         winner = fields[1];
+        document.getElementById('linie-3').style.transform='scaleX(1) rotate(90deg)';
+
     }
 
     if (fields[2]== fields[5] && fields[5] == fields[8]&& fields[2]){
         winner = fields[2];
+        document.getElementById('linie-5').style.transform='scaleX(1) rotate(90deg)';
+
     }
 
-    if (fields[0]== fields[4] && fields[4] == fields[7]&& fields[0]){
+    if (fields[0]== fields[4] && fields[4] == fields[8]&& fields[0]){
         winner = fields[0];
+        document.getElementById('linie-6').style.transform='scaleX(1) rotate(45deg)';
+
     }
 
     if (fields[2]== fields[4] && fields[4] == fields[6]&& fields[2]){
         winner = fields[2];
+        document.getElementById('linie-7').style.transform='scaleX(1) rotate(135deg)';
+
     }
 
     console.log('Winner', winner);
